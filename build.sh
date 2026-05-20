f719cbb14a17474b7b687f7f4aabdca29e216956
Stefano Filippi <103889570+Stefa008@users.noreply.github.com>
2026-05-20 16:35:47 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 sudo apt update
-sudo apt install -y sudo git android-sdk-platform-tools python-is-python3 python3-yaml qemu-utils # libncurses5
-sudo apt install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick protobuf-compiler python3-protobuf lib32readline-dev lib32z1-dev libdw-dev libelf-dev lz4 libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+sudo apt install -y sudo git android-sdk-platform-tools python-is-python3 python3-yaml qemu-utils
+sudo apt install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg
 sudo apt install -y meson-1.5 glslang-tools python3-mako
 git config --global user.name "github-actions[bot]"
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
@@ -17,7 +17,13 @@ export PATH="$(realpath .)/bin:$PATH"
 cd android/lineage
 export PATH="$(realpath .)/prebuilts/sdk/tools/linux/bin/:$PATH"
 repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs --no-clone-bundle
-repo sync -j 8 # $(nproc)
+repo sync -j 8
+
+# === APPLICAZIONE CONFIGURAZIONE 128 GB ===
+find device/ -name "BoardConfig*.mk" -exec sed -i 's/BOARD_SUPER_PARTITION_SIZE := .*/BOARD_SUPER_PARTITION_SIZE := 137438953472/g' {} +
+find device/ -name "BoardConfig*.mk" -exec sed -i 's/BOARD_LINEAGE_DYNAMIC_PARTITIONS_SIZE := .*/BOARD_LINEAGE_DYNAMIC_PARTITIONS_SIZE := 137434759168/g' {} +
+# ==========================================
+
 sed -i 's/-$(LINEAGE_BUILDTYPE)/-jqssun/g' vendor/lineage/config/version.mk
 
 source build/envsetup.sh
@@ -28,5 +34,5 @@ echo "CONFIG_RTC_CLASS=y" >> kernel/virt/virtio/arch/arm64/configs/lineageos/vir
 breakfast virtio_arm64only userdebug
 m recoveryimage
 mv out/target/product/virtio_arm64only/recovery.img ../../recovery-userdebug.img
-breakfast virtio_arm64only user # breakfast virtio_arm64only
+breakfast virtio_arm64only user
 m vm-utm-zip otapackage
